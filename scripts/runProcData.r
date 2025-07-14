b3920fdcd523246f4a6a6d5b4ebdaeb86af9a210
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2025-07-14 13:21:03 -0400

    use TCGA HUB to doubload RNA-seq TPM data

--- scripts/runProcData.r
+++ scripts/runProcData.r
@@ -6,21 +6,21 @@ library(dplyr)
 library(data.table)
 library(MultiAssayExperiment)
 library(S4Vectors)
+library(biomaRt)
 library(qs)
 
 dir_in <- 'data/raw'
 dir_out <- 'data/proc'
 
 ###############################################################
-## load TCGA PAAD data 
+## load TCGA PAAD and clinical data  
 ##############################################################
 # -- Load UCSCXenaTools metadata
 data(XenaData)
 xd <- as.data.table(XenaData)
 cohort <- "TCGA Pancreatic Cancer (PAAD)"
 
-# -- Get RNA-seq (TPM) and RPPA dataset identifiers
-rna_datasets <- xd[XenaCohorts == cohort & grepl("HiSeqV2", XenaDatasets), XenaDatasets]
+# -- Get RPPA dataset identifiers
 rppa_datasets <- xd[XenaCohorts == cohort & grepl("RPPA", DataSubtype), XenaDatasets]
 
 # -- Get clinical matrix
@@ -32,15 +32,7 @@ clinical_data <- XenaGenerate(subset = XenaCohorts == cohort & Type == "clinical
   XenaDownload(destdir = "local_data", download_probeMap = TRUE) |>
   XenaPrepare()
 
-class(clinical_data$PAAD_clinicalMatrix)
-
-# -- Download RNA-seq data
-rna_data <- XenaGenerate(subset = XenaDatasets %in% rna_datasets) |>
-  XenaQuery() |>
-  XenaDownload(destdir = "local_data", download_probeMap = TRUE) |>
-  XenaPrepare()
-
-# -- Download RPPA data
+# -- Download RPPA data  
 rppa_data <- XenaGenerate(subset = XenaDatasets %in% rppa_datasets) |>
   XenaQuery() |>
   XenaDownload(destdir = "local_data", download_probeMap = TRUE) |>
@@ -58,21 +50,10 @@ setkeyv(sData, "sample")
 
 merged_cData <- sData[cData, on = "sample"] # 196 samples with clinical data
 
-# Load only RNA-seq expression and gene annotation
-rna_expr <- as.data.frame(rna_data[["HiSeqV2.gz"]])
-rownames(rna_expr) <- rna_expr$sample
-rna_mat <- rna_expr[, -1] # 183 samples
-
-# -- Build SummarizedExperiment objects (RNA)
-rna_se <- SummarizedExperiment(
-  assays = list(expr = rna_mat),
-  colData = merged_cData[colnames(rna_mat), , drop = FALSE]
-)
-
 # -- Prepare RPPA matrix
 rppa_data <- as.data.frame(rppa_data)
 rownames(rppa_data) <- rppa_data$sample
-rppa_mat <- rppa_data[, -1]
+rppa_mat <- rppa_data[, -1] # 229 proteins and 123 samples
 
 # -- Create annotation file for RPPA data
 annot <- read.csv(file = file.path(dir_in, 'MCLP_Proteins.csv'))
@@ -93,13 +74,49 @@ write.csv(df, file = file.path(dir_out, 'protein_name.csv'), row.names= FALSE)
 rppa_mat <- rppa_mat[rownames(rppa_mat) %in% df$protein, ]
 rppa_mat <- rppa_mat[order(rownames(rppa_mat)), ]
 df <- df[order(df$protein), ]
-rownames(rppa_mat) <- df$updateProtein
+rownames(rppa_mat) <- df$updateProtein # 219 proteins and 123 samples
 
 # -- Build SummarizedExperiment objects (RPPA)
 rppa_se <- SummarizedExperiment(
   assays = list(expr = rppa_mat),
   colData = merged_cData[colnames(rppa_mat), , drop = FALSE],
   rowData = df
+)
+
+################################################################
+## load RNA-seq count data using TCGA HUB
+## download manually using 'TCGA Hub' using 'https://xenabrowser.net/datapages/?cohort=GDC%20TCGA%20Pancreatic%20Cancer%20(PAAD)&removeHub=https%3A%2F%2Fxena.treehouse.gi.ucsc.edu%3A443'
+################################################################ 
+# -- Download RNA-seq TPM data
+rna_data <- read.delim(file = file.path(dir_in, 'TCGA-PAAD.star_tpm.tsv'), stringsAsFactors = FALSE)
+
+# Remove the version numbers in Ensembl ID.
+rna_data$Ensembl_ID <- substring(rna_data$Ensembl_ID, 1, 15)
+rna_data <- rna_data[!duplicated(rna_data$Ensembl_ID), ]
+rownames(rna_data) <- rna_data$Ensembl_ID
+rna_data <- rna_data[, -1]
+colnames(rna_data) <- gsub("\\.", "-", colnames(rna_data))
+colnames(rna_data) <- substr(colnames(rna_data), 1, nchar(colnames(rna_data))-1)
+
+# gene annotation
+ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
+gene_info <- getBM(
+  attributes = c("ensembl_gene_id", "hgnc_symbol"),
+  filters = "ensembl_gene_id",
+  values = rownames(rna_data),
+  mart = ensembl
+)
+
+gene_info <- gene_info[!duplicated(gene_info$hgnc_symbol), ]
+annot <- gene_info[!duplicated(gene_info$ensembl_gene_id), ]
+rna_mat <- rna_data[rownames(rna_data) %in% annot$ensembl_gene_id, ]
+rownames(rna_mat) <- annot$hgnc_symbol
+
+# -- Build SummarizedExperiment objects (RNA)
+rna_se <- SummarizedExperiment(
+  assays = list(expr = rna_mat),
+  colData = merged_cData[colnames(rna_mat), , drop = FALSE],
+  rowData = annot
 )
 
 # -- Create MultiAssayExperiment
